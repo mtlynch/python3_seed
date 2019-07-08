33ae165857531b5ddbfdbb57edbe34a6b91e2005
Michael Lynch <mtlynch@users.noreply.github.com>
2019-07-07 22:06:57 -0500

    Adding logic to exercise the unit test (#14)

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,8 +1,10 @@
 import unittest
 
+from app import dummy
+
 
 class DummyTest(unittest.TestCase):
     """Replace this with a real unit test class."""
 
     def test_dummy(self):
-        self.assertTrue(True)
+        self.assertEqual('dummy', dummy.dummy())
