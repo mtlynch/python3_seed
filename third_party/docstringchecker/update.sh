3126a2283ebf57c448e83fe7cb94e1a35ec38aff
Michael Lynch <mtlynch@users.noreply.github.com>
2019-11-30 18:58:23 -0600

    Upgrading to pylint 1.9.4 and the latest DocStringChecker (#32)
    * Upgrading to pylint 1.9.4 and the latest DocStringChecker
    
    * Fixing location of memoize file

--- third_party/docstringchecker/update.sh
+++ third_party/docstringchecker/update.sh
@@ -10,11 +10,20 @@ set -x
 # Treat undefined variables as errors.
 set -u
 
-OUTPUT_DIR=$(dirname "$0")
-OUTPUT_FILE="${OUTPUT_DIR}/lint.py"
+LINT_OUTPUT_DIR=$(dirname "$0")
+LINT_OUTPUT_FILE="${LINT_OUTPUT_DIR}/lint.py"
 
 wget \
   https://chromium.googlesource.com/chromiumos/chromite/+/master/cli/cros/lint.py?format=TEXT \
   -O - | \
   base64 --decode \
-  > "$OUTPUT_FILE"
+  > "$LINT_OUTPUT_FILE"
+
+MEMOIZE_OUTPUT_DIR=$(dirname "$0")/chromite/utils
+MEMOIZE_OUTPUT_FILE="${MEMOIZE_OUTPUT_DIR}/memoize.py"
+
+wget \
+  https://chromium.googlesource.com/chromiumos/chromite/+/master/utils/memoize.py?format=TEXT \
+  -O - | \
+  base64 --decode \
+  > "$MEMOIZE_OUTPUT_FILE"
